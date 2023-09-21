af0cd09bf7e4998731fd792667cca486e0a4cbc4
Nikhil Shrivas <87777902+nshri1609@users.noreply.github.com>
2023-09-21 11:32:10 +0530

    Add files via upload

---
+++ contracts/Georli_Testnet.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+
+contract Georli_Testnet {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit_Georli(uint256 amount);
+    event Withdraw_Georli(uint256 amount);
+   
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balance;
+    }
+
+    function Deposit_Georli(uint256 _amount) public payable {
+        uint256 _previousBalance = balance;
+
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // perform transaction
+        balance += _amount;
+
+        // assert transaction completed successfully
+        assert(balance == _previousBalance + _amount);
+
+        // emit the event
+        emit Deposit_Georli(_amount);
+    }
+
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint256 _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        // withdraw the given amount
+        balance -= _withdrawAmount;
+
+        // assert the balance is correct
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        // emit the event
+        emit Withdraw_Georli(_withdrawAmount);
+    }
+
+    function getContractAddress() public view returns (address) {
+        return address(this);
+    }
+
+}
